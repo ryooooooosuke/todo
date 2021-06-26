46e189287438375bbfa2d3dae50bfa73fadacd2e
ryoosuke1992 <nabesuke1992@gmail.com>
2021-06-26 15:44:45 +0900

    chapter 6

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateF
 Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
 Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
+Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
